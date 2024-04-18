5ff9cef1f0339b86c5613733d8f563b4bee6eea5
Kevin Kliimask <kevin.kliimask@ut.ee>
2024-04-18 22:13:12 +0300

    implement openai service

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, UploadFile, HTTPException
 
-from chatgpt_service import handle_tagging
+from openai_service import handle_tagging
+
 
 app = FastAPI()
 
 @app.post("/")
-async def get_tags(file: UploadFile):
+async def get_tags(file: UploadFile, count: int = 5):
     if file.content_type != "text/csv":
         raise HTTPException(status_code=415, detail="Unsupported content-type")
 
-    tags = await handle_tagging(file)
-    return {"tags": tags}
+    return await handle_tagging(file, count)
---
+++ openai_service.py
@@ -0,0 +1,44 @@
+import codecs
+import csv
+from typing import Literal, List, TypedDict, Dict
+from fastapi import UploadFile
+from openai import OpenAI
+
+from config import Settings
+
+settings = Settings()
+client = OpenAI(api_key=settings.chatgpt_api_key)
+
+
+class TagsData(TypedDict):
+    english: List[str]
+    estonian: List[str]
+
+
+async def handle_tagging(file: UploadFile, count: int) -> Dict[Literal["data"], TagsData]:
+    reader = csv.reader(codecs.iterdecode(file.file, "utf-8"))
+
+    messages = [{"role": "system", "content": "You will generate tags for a dataset. I will provide your the first rows"
+                                              "of the dataset, whereas the very first row will be the column titles of the dataset."
+                                              "The first row will be in the following form: title1,title2,title3,etc..."
+                                              "The next rows will be in the following form: value1,value2,value3,etc..."
+                                              f"Output {count} tags that describe the dataset best. Output only the "
+                                              "suitable tags in the form of: tag1,tag2,tag3,etc... Tags should be in English"}]
+
+    i = 0
+    user_message = ""
+    for row in reader:
+        if i == 10:
+            break
+        user_message += (",".join(row) + "\n")
+        i += 1
+    messages.append({"role": "user", "content": user_message})
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    english_tags = response.choices[0].message.content.split(",")
+
+    estonian_tags = ["empty"]
+
+    return {"data": {"english": english_tags, "estonian": estonian_tags}}
