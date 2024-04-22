b06bcce5132e9298b5da1b3166fb7c252d52b45a
Kevin Kliimask <kevin.kliimask@ut.ee>
2024-04-23 01:13:34 +0300

    comment out env var

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     frontend_url: str
     chatgpt_api_key: str
-    google_application_credentials: str
+    # google_application_credentials: str
     deepl_auth_key: str
 
 
